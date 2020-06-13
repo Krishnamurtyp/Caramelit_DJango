201a23d479586f1a4146e6d2785d090d71ab4105
Vinesh <Vinesh.katewa@gmail.com>
2020-06-13 10:37:54 +0530

    Refactored the code to make it more readable and changed database to sqlite for testing

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'djangoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'ssyo',
-        'USER':'postgres',
-        'PASSWORD':'unibiophy',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
