475e8503a5ed3f9df157ea50592a0cdce580a883
Vinesh <Vinesh.katewa@gmail.com>
2020-07-02 12:55:27 +0530

    Added mongodb connection

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'djangoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': '<dbname>',
+        'HOST': 'mongodb://<dbuser>:<dbpass>@test-cluster-t8qxz.mongodb.net/<dbname>?retryWrites=true',
+        'USER': '<dbuser>',
+        'PASSWORD': '<dbpass>',
     }
 }
 
